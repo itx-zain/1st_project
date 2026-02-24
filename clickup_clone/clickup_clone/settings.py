ed2ad478704fd0c7a7cfdb3483907c4ec37f4581
Zain Tanveer <zain.tanveer@example.com>
2026-02-24 10:57:43 +0500

    I setup PostgreSQL database

--- clickup_clone/clickup_clone/settings.py
+++ clickup_clone/clickup_clone/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'clickup_clone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'file_management_db',
+        'USER': 'file_user',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
