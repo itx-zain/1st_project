909c9a9345e41bb5cbae5523691a5b4ea1dd9e3a
Zain Tanveer <zain.tanveer@example.com>
2026-02-26 14:31:44 +0500

    Update settings and urls

--- clickup_clone/clickup_clone/settings.py
+++ clickup_clone/clickup_clone/settings.py
@@ -147,7 +147,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 
 }
